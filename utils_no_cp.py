c1832031b2e4923bb5f18502090e89c378608c02
MARQUER Esteban <esteban.marquer@loria.fr>
2022-02-21 18:31:33 +0100

    add augmentation with central permutation not accepted as a predicate

--- utils_no_cp.py
+++ utils_no_cp.py
@@ -1,19 +1,17 @@
 import random
-"""File containing generic data manipulation methods for analogy-oriented datasets."""
+"""File containing generic data manipulation methods for analogy-oriented datasets, when central permutation is not accepted."""
 
 def enrich(a, b, c, d):
-    """Apply the example generation process from 'Solving Word Analogies: a Machine Learning Perspective'."""
+    """Apply the example generation process from 'Solving Word Analogies: a Machine Learning Perspective', modified to exclude central permutation."""
     yield a, b, c, d
     yield c, d, a, b
-    yield c, a, d, b
-    yield d, b, c, a
     yield d, c, b, a
     yield b, a, d, c
-    yield b, d, a, c
-    yield a, c, b, d
 
 def generate_negative(a, b, c, d):
-    """Apply the negative example generation process from 'Solving Word Analogies: a Machine Learning Perspective'."""
+    """Apply the negative example generation process from 'Solving Word Analogies: a Machine Learning Perspective', modified to include central permutation."""
+    #previously accepted by central perm
+    yield a, c, b, d # implied by the enrichment d, b, c, a | b, d, a, c | c, a, d, b
     yield b, a, c, d
     yield c, b, a, d
     yield a, a, c, d
@@ -21,7 +19,7 @@ def generate_negative(a, b, c, d):
 def random_sample_negative(a, b, c, d, filter_invalid=True, tensors=True, n=8):
     """Randomly sample invalid analogies from the available negative forms.
     
-    In this variant, central permutation is accepted.
+    In this variant, central permutation is not accepted.
     
     :param filter_invalid: If True, will exclude quadruples from negative forms if the an identical quadruple is already present in the positive forms.
     :param tensors: If True, consider that a, b, c, and d are tensors and use an appropriate equality check for quadruples.
@@ -32,8 +30,8 @@ def random_sample_negative(a, b, c, d, filter_invalid=True, tensors=True, n=8):
 
 def n_pos_n_neg(a, b, c, d, filter_invalid=True, filter_valid=False, tensors=True, n=8):
     """Generate positive examples (including the base form) and negative examples form a base analogical form.
-    
-    In this variant, central permutation is accepted.
+
+    In this variant, central permutation is not accepted.
 
     Returns two lists, one of positive forms, the other of negative forms.
 
